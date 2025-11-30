4f21a372bd21971ad8a4a9c0f42696475fe2b71f
MejriZiad <mejri.ziad@gmail.com>
2025-11-30 18:19:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Arabic Hub Education
-const CACHE_NAME = 'arabic-hub-v1.0.1';
-const STATIC_CACHE = 'arabic-static-v1.1.1';
-const DYNAMIC_CACHE = 'arabic-dynamic-v1.1.1';
+const CACHE_NAME = 'arabic-hub-v1.0.2';
+const STATIC_CACHE = 'arabic-static-v1.1.2';
+const DYNAMIC_CACHE = 'arabic-dynamic-v1.1.2';
 
 // Assets to cache immediately on install
 const STATIC_ASSETS = [
@@ -220,4 +220,5 @@ self.addEventListener('error', (event) => {
 
 self.addEventListener('unhandledrejection', (event) => {
     console.error('Service Worker: Unhandled promise rejection', event.reason);
+
 });
