f40d4242c5018f12c468c0f78fad833e9a21f40b
MejriZiad <mejri.ziad@gmail.com>
2025-11-30 23:51:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Arabic Hub Education
-const CACHE_NAME = 'arabic-hub-v1.0.2';
-const STATIC_CACHE = 'arabic-static-v1.1.2';
+const CACHE_NAME = 'arabic-hub-v1.0.1';
+const STATIC_CACHE = 'arabic-static-v1.1.1';
 const DYNAMIC_CACHE = 'arabic-dynamic-v1.1.2';
 
 // Assets to cache immediately on install
@@ -220,5 +220,4 @@ self.addEventListener('error', (event) => {
 
 self.addEventListener('unhandledrejection', (event) => {
     console.error('Service Worker: Unhandled promise rejection', event.reason);
-
 });
