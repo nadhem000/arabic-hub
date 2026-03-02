f6de563b58edcdc9de3e96aa4c5c15dcc9b1bccb
MejriZiad <mejri.ziad@gmail.com>
2026-03-02 02:12:44 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Arabic Hub Education
-const CACHE_NAME = 'arabic-hub-v1.2.5';
-const STATIC_CACHE = 'arabic-static-v1.2.5';
-const DYNAMIC_CACHE = 'arabic-dynamic-v1.2.5';
+const CACHE_NAME = 'arabic-hub-v1.2.6';
+const STATIC_CACHE = 'arabic-static-v1.2.6';
+const DYNAMIC_CACHE = 'arabic-dynamic-v1.2.6';
 
 // Assets to cache immediately on install
 const STATIC_ASSETS = [
